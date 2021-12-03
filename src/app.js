54e6e3331e76cb7e083a98f9d16b30d218993e8b
Fatih Yavuz <ibrahim.fatih.yavuz@gmail.com>
2021-12-03 14:22:15 +0100

    modified css file

--- src/app.js
+++ src/app.js
@@ -1,28 +1,32 @@
 const express = require('express')
 const path = require('path')
+const hbs  = require('hbs')
 
 const app = express()
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname,'../public')
-const viewsPath = path.join(__dirname,'../templates')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialPath = path.join(__dirname,'../templates/partials')
 
 // Setup handlebars engine and views location
 app.set('view engine','hbs')
 app.set('views',viewsPath)
+hbs.registerPartials(partialPath)
 
 // Setup static directory to serve
 app.use(express.static(publicDirectoryPath))
 
 app.get('',(req,res)=>{
     res.render('index',{
-        title:'Weather App',
+        title:'Weather',
         name:'Fatih Yavuz'
     })
 })
 
 app.get('/help',(req,res)=>{
     res.render('help',{
+        helpText:'Some help text',
         title: 'Help',
         name: 'Fatih Yavuz'
     })
@@ -41,6 +45,27 @@ app.get('/weather',(req,res)=>{
         forecast: 5
     })
 })
+
+// handle 404 pages 
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title:'404',
+        name: 'Fatih Yavuz',
+        errorMessage:'Help article not found'
+        
+    })
+})
+
+app.get('*',(req,res)=>{
+    res.render('404',{
+        title:404,
+        name: 'Fatih Yavuz',
+        errorMessage:'Page Not Found!'
+        
+    })
+})
+
 
 app.listen(3000,()=>{
     console.log('The server is up on port 3000')
