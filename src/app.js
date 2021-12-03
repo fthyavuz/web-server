0582ed48b6d37074d442c902e1daeb8b357a8f56
Fatih Yavuz <ibrahim.fatih.yavuz@gmail.com>
2021-12-03 20:24:09 +0100

    added API endpoint

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const hbs  = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
 
 const app = express()
 
@@ -40,12 +43,51 @@ app.get('/about',(req,res)=>{
 })
 
 app.get('/weather',(req,res)=>{
-    res.send({
-        location: 'Ankara',
-        forecast: 5
+
+    const address = req.query.address
+    if(!address){
+        return res.send({
+            error:'You must provide an address!'
+        })
+    }
+
+    
+    geocode(address,(error,{latitude,longtitude,placeName}={})=>{
+        if (error){
+            return res.send({
+                errorMessage: error
+            })  
+        }
+    
+        forecast(latitude,longtitude,(error,forecastData)=>{
+            if(error){
+                return res.send({
+                    errorMessage:error
+                }) 
+            }
+
+            res.send({
+                forecast: forecastData,
+                location: placeName,
+                address:  address
+            })
+
+        })
     })
 })
 
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            error: 'You must provide a search term'
+        })
+    }
+    
+    console.log(req.query.search)
+    res.send({
+        products:[]
+    })
+})
 // handle 404 pages 
 
 app.get('/help/*',(req,res)=>{
---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('postman-request')
+
+const access_key = '95f68b3d37c937f9d7ffec101cc4e715'
+
+const forecast = (lat,lon,callback) => {
+    url = 'http://api.weatherstack.com/current?access_key=' + access_key + '&query=' + lat + ',' + lon
+    request({url,json:true},(error,{ body }) => {
+        if(error){
+            callback('Unable to connect to weather service',undefined)
+        } else if(body.error){
+            callback('Unable to find location',undefined)
+        } else {
+                const temp = body.current.temperature
+                const flike = body.current.feelslike
+                const description = body.current.weather_descriptions[0]
+            callback(undefined,description+' .It is currently '+ temp + ' degrees out. It feels like ' + flike + ' degrees out.')
+        }
+    })
+}
+
+module.exports = forecast
